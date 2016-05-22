a6e9d6e371f29a93209fe9e3c87d491b4d1efafa
MIke Nancett <MNancett@MIkes-MacBook-Pro.local>
2016-05-22 16:59:09 -0500

    Message

--- Tax Calc 2/ViewController.m
+++ Tax Calc 2/ViewController.m
@@ -9,6 +9,14 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *priceTextField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+- (IBAction)onCalculateButtonTapped:(id)sender;
+@property double sfTax;
+@property double chiTax;
+@property double nycTax;
+@property double total;
 
 @end
 
@@ -16,12 +24,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.sfTax = 0.0875;
+    self.chiTax= 0.1025;
+    self.nycTax = 0.08875;
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
+
+- (IBAction)onCalculateButtonTapped:(id)sender {
+    NSString *priceFromUser = self.priceTextField.text;
+    int enteredValue = priceFromUser.intValue;
+    
+    if (self.segmentedControl.selectedSegmentIndex == 0){
+        double result = enteredValue * self.sfTax;
+        double total = result + enteredValue;
+        
+        self.resultLabel.text = [NSString stringWithFormat: @"The tax on $%d would come to $%.2f. The total for the item would be $%.2f in San Francisco.", enteredValue, result, total];
+    }else if (self.segmentedControl.selectedSegmentIndex == 1){
+        double result = enteredValue * self.chiTax;
+        double total = result + enteredValue;
+        
+        self.resultLabel.text = [NSString stringWithFormat: @"The tax on $%d would come to $%.2f. The total for the item would be $%.2f in Chicago.", enteredValue, result, total];
+
+    }else {
+        double result = enteredValue * self.nycTax;
+        double total = result + enteredValue;
+        
+        self.resultLabel.text = [NSString stringWithFormat: @"The tax on $%d would come to $%.2f. The total for the item would be $%.2f in New York.", enteredValue, result, total];
+    }
+}
 @end
